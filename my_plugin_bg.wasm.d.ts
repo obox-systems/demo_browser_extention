836019019777e3c7ada200335a2e9de9c5cd9daa
Sakapoi <97370653+Sakapoi@users.noreply.github.com>
2023-05-15 15:59:13 +0300

    Delete my_plugin_bg.wasm.d.ts

--- my_plugin_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function my_plugin_function(): void;
-export function run(): void;
-export const __wbindgen_export_0: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h161bda1e1e411cf1(a: number, b: number): void;
-export function __wbindgen_exn_store(a: number): void;
-export function __wbindgen_start(): void;
