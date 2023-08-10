91e0c2c9e20cefc4ab8007e841ed8b15dba3804b
Sakapoi <97370653+Sakapoi@users.noreply.github.com>
2023-08-10 11:27:47 +0000

    added lints and documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+#![warn(rust_2018_idioms)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+
+//! In this project, I developed a simple Google Chrome extension that prints the URLs of all opened tabs of the browser in the extension HTML window, when the button of the extension is pressed. This extension was written in Rust and compiled to WASM to use the Rust functions in the Javascript module of the extension later.
+//! 
+//! In this project all Rust functions are in the src folder, they perform the main logic of a plugin. manifest.json is the file with all settings of the plugin and is the first file that our browser sees after installing the plugin. popup.html is a file with HTML markup of a plugin window. And the script.js file contains Javascript functions that are exported into Rust.
+//! 
+//! I used a wasm-bindgen crate to compile functions from Rust module into a wasm file. I also used a web-sys crate to get the information about the URLs of all opened tabs of the browser.
+//! 
+//! It can be used to get a list of all open tabs in your browser and will be useful if you have a lot of tabs open and want to find a particular one.
+
 use wasm_bindgen::prelude::*;
 
 macro_rules! console_log {
@@ -16,6 +28,7 @@ extern "C" {
     fn changeContent(content: JsValue);
 }
 
+/// this function takes callback from js function and transforms it to pretty view
 #[wasm_bindgen]
 pub fn my_plugin_function() {
     // let closure = Closure::wrap(Box::new(|url: JsValue| {
